b4c3ef6a8df25fc19f55a68a64c4b69640fbb3bf
yas-wael <y.alwael@gmail.com>
2022-08-02 17:08:39 +0300

    cw 2

---
+++ My project/Assets/thebadassvillain.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thebadassvillain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string villainname = "handsome";
+        int villainage = 21;
+        float villainheight = 1.89f;
+        string villainsuperpower = "none";
+
+        print("my name is" + villainname + "and i will be your destruction" + "and my age is " + villainage + "and my height is" + villainheight + "you short" + "and my super power is" + villainsuperpower + "but im the most intillegnt human alive");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ My project/Assets/theuselesshero.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class theuselesshero : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string heroname = "useless";
+        int heroage = 16;
+        float heroheight = 1.17f;
+        string herosuperpower = "friendship";
+
+
+        print("my name is" + heroname + "and my age is" + heroage + "and my height is" + heroheight + "and my super power is" + herosuperpower);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
